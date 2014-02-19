171eeed0ff393e5ec218346939c6c39e3f556f39
Diyah <me@hadiyah.me>
2014-02-19 11:20:14 -0800

    wip

--- package.js
+++ package.js
@@ -2,12 +2,25 @@ Package.describe({
   summary: "Login service for mailchimp accounts"
 });
 
-Package.on_use(function (api, where) {
-  api.add_files('accounts-mailchimp.js', ['client', 'server']);
-});
+Package.on_use(function(api) {
+    api.use('accounts-base', ['client', 'server']);
+    api.imply('accounts-base', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
 
-Package.on_test(function (api) {
-  api.use('accounts-mailchimp');
+    api.use('oauth', ['client', 'server']);
+    api.use('oauth2', ['client', 'server']);
+    api.use('http', ['server']);
+    api.use('underscore', 'server');
+    api.use('templating', 'client');
+    api.use('random', 'client');
+    api.use('service-configuration', ['client', 'server']);
+          
+    api.add_files(
+    ['lib/mailchimp_configuration.html', 'lib/mailchimp_configuration.js', 
+    'lib/mailchimp_login_button.css'],
+    'client');
 
-  api.add_files('accounts-mailchimp_tests.js', ['client', 'server']);
+    api.add_files("lib/accounts_mailchimp.js");
+    api.add_files('lib/mailchimp_client.js', 'client');
+    api.add_files('lib/mailchimp_server.js', 'server');
 });
